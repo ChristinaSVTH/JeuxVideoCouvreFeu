4d6bf73a020c5b8494193410d66d3ca724c33962
ChristinaSVTH <christina.sisavath@gmail.com>
2021-10-11 17:07:28 +0200

    responsive 52%

--- script.js
+++ script.js
@@ -46,6 +46,56 @@ window.onclick = function(event) {
   }
 }
 
+// RESPONSIVE
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtnR");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+var modal1 = document.getElementById("myModal1");
+var btn1 = document.getElementById("myBtn1R");
+var span = document.getElementsByClassName("close1")[0];
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+span.onclick = function() {
+  modal1.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+var modal2 = document.getElementById("myModal2");
+var btn2 = document.getElementById("myBtn2R");
+var span = document.getElementsByClassName("close2")[0];
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+span.onclick = function() {
+  modal2.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+
 
 // HAMBURGER
 
